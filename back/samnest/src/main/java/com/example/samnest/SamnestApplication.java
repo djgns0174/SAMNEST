d01b1775730dd2323f7d80306892f8db0dc0cac2
djgns0174 <74578132+djgns0174@users.noreply.github.com>
2024-07-03 21:46:02 +0900

    setting

---
+++ back/samnest/src/main/java/com/example/samnest/SamnestApplication.java
@@ -0,0 +1,13 @@
+package com.example.samnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamnestApplication.class, args);
+	}
+
+}
---
+++ back/samnest/src/test/java/com/example/samnest/SamnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
