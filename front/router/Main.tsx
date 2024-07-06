704d9d785ad007450a8bb87c5189851230a6cb2b
ooosj <tmdwn8775@gmail.com>
2024-07-07 01:37:48 +0900

    Update code1

--- front/router/Main.tsx
+++ front/router/Main.tsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import { View, Text, Button } from 'react-native';
-// import { StackNavigationProp } from '@react-navigation/stack';
-// import { RootStackParamList } from '../App';
-
-// type MainScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Main'>;
-
-// type Props = {
-//   navigation: MainScreenNavigationProp;
-// };
-
-// const Main: React.FC<Props> = ({ navigation }) => {
-//   return (
-//     <View>
-//       <Text>Main Screen</Text>
-//       <Button
-//         title="Go to Map"
-//         onPress={() => navigation.navigate('Map')}
-//       />
-//     </View>
-//   );
-// };
-
-// export default Main;
-
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
--- front/router/Map.tsx
+++ front/router/Map.tsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import { View, Text, Button } from 'react-native';
-// import { StackNavigationProp } from '@react-navigation/stack';
-// import { RootStackParamList } from '../App';
-
-// type MapScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Map'>;
-
-// type Props = {
-//   navigation: MapScreenNavigationProp;
-// };
-
-// const Map: React.FC<Props> = ({ navigation }) => {
-//   return (
-//     <View>
-//       <Text>Map Screen</Text>
-//       <Button
-//         title="Go Back to Main"
-//         onPress={() => navigation.navigate('Main')}
-//       />
-//     </View>
-//   );
-// };
-
-// export default Map;
-
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
